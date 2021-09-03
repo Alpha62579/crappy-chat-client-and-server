bea07ad368ccd014c6f05d7618db1e73d2ff1eb0
surya62579 <50741530+surya62579@users.noreply.github.com>
2021-09-03 14:48:01 +0530

    Committed the damn files

---
+++ client.py
@@ -0,0 +1,75 @@
+import json
+import socket
+import threading
+import sys
+
+from prompt_toolkit import PromptSession
+from prompt_toolkit.patch_stdout import patch_stdout
+
+stop = True
+HEADER = 256
+PORT = 62579
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.137.1"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+name = input("Enter a name: ")
+chatroom = input("Enter a chatroom name: ")
+password = input(
+    "Enter a password, this will be used for authentication if the chatroom doesn't exist or to join the existing "
+    "chatroom: ")
+packet_data = json.dumps({"type": "packet.identify", "name": name, "room": chatroom, "password": password})
+client.send(packet_data.encode(FORMAT))
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = json.dumps({"type": "packet.message", "length": msg_length, "room": chatroom})
+    client.send(send_length.encode(FORMAT))
+    client.send(message)
+
+
+def on_message(conn):
+    global stop
+    while stop:
+        try:
+            msg_length = conn.recv(HEADER).decode(FORMAT)
+        except ConnectionResetError:
+            print("Server closed.")
+            stop = False
+            sys.exit(0)
+        if msg_length:
+            msg_length = json.loads(msg_length)
+            if msg_length['type'] == 'packet.kick':
+                print("You got kicked from this chatroom.")
+                stop = False
+                sys.exit(0)
+            msg = conn.recv(msg_length['length']).decode(FORMAT)
+            print(f"Message received from {msg_length['name']}: {str(msg)}")
+
+
+def main():
+    global stop
+    session = PromptSession()
+    while stop:
+        try:
+            with patch_stdout():
+                text = session.prompt("> ")
+                if text != '':
+                    if text == '//disconnect':
+                        raise KeyboardInterrupt
+                    send(text)
+        except KeyboardInterrupt:
+            send(DISCONNECT_MESSAGE)
+            sys.exit(0)
+
+
+if __name__ == '__main__':
+    threading.Thread(target=on_message, args=(client,), daemon=True).start()
+    main()
+
---
+++ server.py
@@ -0,0 +1,146 @@
+import json
+import os
+import socket
+import sys
+import threading
+import time
+
+PORT = os.getenv("PORT", 62579)
+SERVER = "0.0.0.0"
+ADDR = (SERVER, PORT)
+
+HEADER = 256
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "//disconnect"
+KICK_MESSAGE = "/kick"
+
+chatrooms = {}
+stay = True
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    global stay
+    global chatrooms
+    while stay:
+        try:
+            raw_message = conn.recv(HEADER).decode(FORMAT)
+        except:
+            for i, room in enumerate(chatrooms):
+                if conn in chatrooms[room]['connections']:
+                    break
+            trigger_message(f"{chatrooms[room.lower()]['connections'][conn]} has disconnected.", "SERVER", chatrooms[room.lower()]['connections'])
+            print(f"{chatrooms[room.lower()]['connections'][conn]}:{addr} has disconnected.")
+            break
+        if raw_message:
+            raw_json = json.loads(raw_message)
+            room = raw_json['room']
+            if raw_json['type'] == 'packet.message':
+                msg_length = raw_json['length']
+                msg = conn.recv(msg_length).decode(FORMAT)
+                if msg == DISCONNECT_MESSAGE:
+                    name = chatrooms[raw_json['room']]['connections'][conn]
+                    chatrooms[raw_json['room']]['connections'].pop(conn)
+                    try:
+                        trigger_message(f"{name} has disconnected.", "SERVER", chatrooms[room.lower()]['connections'])
+                    except KeyError:
+                        pass
+                    print(f"{name}:{addr} has disconnected.")
+                    if not chatrooms[raw_json['room']]['connections'].keys():
+                        chatrooms.pop(raw_json['room'])
+                    break
+                elif msg.split(" ")[0] == KICK_MESSAGE:
+                    if chatrooms[room.lower()]['connections'][conn] in chatrooms[raw_json['room']]['ops']:
+                        try:
+                            byebyeguy = msg.split(" ")[1]
+                        except IndexError:
+                            send("Enter the name of a person in this chatroom.", conn)
+                            continue
+                        con = None
+                        for i, con in enumerate(chatrooms[room.lower()]['connections']):
+                            if chatrooms[room.lower()]['connections'][con] == byebyeguy:
+                                header = json.dumps({"type": "packet.kick"}).encode(FORMAT)
+                                con.send(header)
+                                con.close()
+                                trigger_message(f"{byebyeguy} has been kicked from this chatroom!", "SERVER", chatrooms[room.lower()]['connections'])
+                        if con:
+                            chatrooms[raw_json['room']]['connections'].pop(con)
+                        else:
+                            send(f"No one by the name of {byebyeguy} is online in the chatroom.", conn)
+                            continue
+                    else:
+                        send("Welp, you aren't an OP in the room.", conn)
+                        continue
+                print(f"[{chatrooms[room.lower()]['connections'][conn]}:{addr}:{raw_json['room'].lower()}] {msg}")
+                trigger_message(msg, chatrooms[room.lower()]['connections'][conn], chatrooms[room.lower()]['connections'])
+            elif raw_json['type'] == 'packet.identify':
+                if raw_json['room'].lower() not in chatrooms:
+                    chatrooms[raw_json['room'].lower()] = {"connections": {conn: raw_json['name']}, "ops": [raw_json['name']], "password": raw_json[
+                        'password'] if "password" in raw_json.keys() else None}
+                    trigger_message(f"{chatrooms[raw_json['room'].lower()]['connections'][conn]} has connected.", "SERVER", chatrooms[room.lower()]['connections'])
+                    send(f"Chatroom {raw_json['room'].lower()} has been created successfully. Password: {raw_json['password']}", conn)
+                    print(f"{chatrooms[room.lower()]['connections'][conn]}:{addr} has connected")
+                    print(f"Created chatroom {raw_json['room'].lower()}")
+                else:
+                    if chatrooms[raw_json['room'].lower()]['password'] == raw_json['password'] or chatrooms[raw_json['room'].lower()]['password'] is None:
+                        for connection in chatrooms[room.lower()]['connections']:
+                            if connection == raw_json['name']:
+                                send("Your name is currently being used by someone else. Please use another name.", conn)
+                        chatrooms[room.lower()]['connections'][conn] = raw_json['name']
+                        trigger_message(f"{chatrooms[room.lower()]['connections'][conn]} has connected.", "SERVER", chatrooms[room.lower()]['connections'])
+                        print(f"{chatrooms[raw_json['room']]['connections'][conn]}:{addr}:{raw_json['room'].lower()} has connected.")
+                    else:
+                        send("Invalid password.", conn)
+                        break
+
+    conn.close()
+    sys.exit(0)
+
+
+def send(msg, conn):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = json.dumps({"type": "packet.message", "length": msg_length, "name": "SERVER"})
+    conn.send(send_length.encode(FORMAT))
+    time.sleep(0.2)
+    conn.send(message)
+
+
+def trigger_message(message, sender, connections):
+    for connection in connections:
+        if connections[connection] != sender:
+            message = str(message)
+            if message.startswith("b'"):
+                message = message[2:-1]
+            message = message.encode(FORMAT)
+            msg_length = json.dumps({"type": "packet.message", "length": len(message), "name": sender})
+            send_length = msg_length.encode(FORMAT)
+            send_length += b' ' * (HEADER - len(send_length))
+            connection.send(send_length)
+            connection.send(message)
+
+
+def start():
+    global stay
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}:{PORT}")
+    while stay:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr), daemon=True)
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 2}")
+    sys.exit(0)
+
+
+print("[SERVER] Starting server.")
+t = threading.Thread(target=start, daemon=True)
+t.start()
+while True:
+    try:
+        input()
+    except KeyboardInterrupt:
+        print("[SERVER] Server stopping.")
+        stay = False
+        sys.exit(0)
